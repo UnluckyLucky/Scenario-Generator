58746962a2c5464ff75c2c40f1636d8ba04becbc
Daniel Lockhart <daniel@lockyy.com>
2015-01-30 20:55:20 +0000

    Make column reroll algorithm insert new sub columns into correct place, rather than just sticking them on the end

--- app/controllers/scenario_controller.rb
+++ app/controllers/scenario_controller.rb
@@ -23,7 +23,6 @@ class ScenarioController < ApplicationController
     @game_name = params[:game_name].gsub(/#/,"").to_sym
     if ScenarioGenerator.games.include? @game_name
       @sub_scenario, @sub_trees_to_remove = ScenarioGenerator.sub_scenario @game_name, params[:column_name].downcase.tr(" ", "_")
-      puts @sub_scenario, @sub_trees_to_remove
       @scenario = construct_hash_from_params
 
       @title = ScenarioGenerator.game_display_name @game_name
@@ -42,18 +41,25 @@ class ScenarioController < ApplicationController
     end
 
     def construct_hash_from_params
-      hash = {}
+      cleared_hash = {}
       params[:existing_data].each do |param_key, param_value|
         unless @sub_trees_to_remove && @sub_trees_to_remove.include?(param_key.to_sym)
-          hash[param_key.titleize] = param_value
+          cleared_hash[param_key.titleize] = param_value
         end
       end
 
-      @sub_scenario.each do |sub_key, sub_value|
-        hash[sub_key.titleize] = sub_value
+      returned_hash = {}
+
+      cleared_hash.each do |cleared_key, cleared_value|
+        if cleared_key.to_sym == @sub_scenario.keys[0].to_sym
+          @sub_scenario.each do |sub_key, sub_value|
+            returned_hash[sub_key.titleize] = sub_value
+          end
+        end
+        returned_hash[cleared_key.titleize] = cleared_value
       end
 
-      return hash
+      return returned_hash
     end
 
 end
