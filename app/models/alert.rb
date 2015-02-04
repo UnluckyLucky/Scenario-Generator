a23aaebbcc211945551f940349e1348783e9ddb7
Daniel Lockhart <daniel@lockyy.com>
2015-02-04 22:28:36 +0000

    Add email validation to email signup form

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,5 +1,7 @@
 class Alert < ActiveRecord::Base
 
+  validates :email, :email => true
+  
   validates_presence_of :email, :message => "required"
   validates_uniqueness_of :email, :message => "already subscribed"
 
