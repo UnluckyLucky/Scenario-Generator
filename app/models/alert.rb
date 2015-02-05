f8aaeb1f5f2bf3596b0e479eb22c3ce0a2a035a3
Daniel Lockhart <daniel@lockyy.com>
2015-02-05 01:43:58 +0000

    Add function to list emails

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,8 +1,12 @@
 class Alert < ActiveRecord::Base
 
   validates :email, :email => true
-  
+
   validates_presence_of :email, :message => "required"
   validates_uniqueness_of :email, :message => "already subscribed"
+
+  def self.email_list
+    all.map { |alert| alert.email }
+  end
 
 end
