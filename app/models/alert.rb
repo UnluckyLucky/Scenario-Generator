5296acfdd7bbcc8fe63b92ec1be538775f5d8ffb
Daniel Lockhart <daniel@lockyy.com>
2015-02-14 00:48:14 +0000

    Update alert functions to make exporting emails easier.

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -5,8 +5,18 @@ class Alert < ActiveRecord::Base
   validates_presence_of :email, :message => "required"
   validates_uniqueness_of :email, :message => "already subscribed"
 
-  def self.email_list
+  def self.all_emails
     all.map { |alert| alert.email }
+  end
+
+  def self.email_list
+    string = ""
+
+    all.each do |alert|
+      string += "#{alert.email}, "
+    end
+
+    return string[0..-3]
   end
 
 end
