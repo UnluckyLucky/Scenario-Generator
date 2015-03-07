b7bfdf654b8277add73e121c8b3d462435929d01
Daniel Lockhart <daniel@lockyy.com>
2015-03-07 02:37:10 +0000

    Replace form on site with mail chimp form

--- app/controllers/alert_controller.rb
+++ app/controllers/alert_controller.rb
@@ -2,34 +2,6 @@ class AlertController < ApplicationController
 
   skip_before_filter :verify_authenticity_token, :only => [:mailchimp_callback]
 
-  def subscribe
-    @alert = Alert.new(email: params[:alert][:email], version: current_version_model, signup_page: params[:page_name])
-
-    if email_submission_allowed?
-      save_alert
-    end
-
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.js
-    end
-  end
-
-  def emails
-    @title = 'Scenario Generator Unsubscribe Form'
-  end
-
-  def unsubscribe
-    @alert = Alert.find_by(email: params[:alert][:email])
-
-    if @alert 
-      @alert.destroy
-    end
-
-    flash[:error] = "#{params[:alert][:email]} successfully removed"
-    redirect_to alert_emails_path
-  end
-
   def mailchimp_callback
     if params[:mailchimp_password] == ENV['MAILCHIMP_PASSWORD']
       if params[:type] == 'unsubscribe' || params[:type] == 'cleaned'
@@ -52,43 +24,5 @@ class AlertController < ApplicationController
       redirect_to root_url
     end
   end
-
-  private
-
-    def save_alert
-      if @alert.save
-        flash[:success] = "#{params[:alert][:email]} successfully saved"
-        flash[:error] = nil
-        session[:email_submissions] += 1
-        session[:last_email_submission_time] = Time.now
-      else
-        flash[:success] = nil
-        flash[:error] = "#{@alert.email} #{@alert.errors[:email][0]}"
-      end
-    end
-
-    def email_submission_allowed?
-      if session[:email_submissions] && session[:last_email_submission_time]
-
-        wait_time = 15.seconds
-        last_suggested_time = session[:last_email_submission_time]
-        remaining_time = wait_time - (Time.now - last_suggested_time)
-
-        if session[:email_submissions] > 2
-          if remaining_time > 0
-            flash[:success] = nil
-            flash[:error] = "You are submitting emails too fast, please wait #{remaining_time.to_i} seconds"
-            return false
-          else
-            session[:email_submissions] = 0
-          end
-        end
-
-      else
-        session[:email_submissions] = 0
-      end
-
-      return true 
-    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,6 @@ DfScenario::Application.routes.draw do
   post 'suggestion/:page_name', to: 'suggestion#submit', as: 'suggestion_submit'
   post 'subscribe/:page_name', to: 'alert#subscribe', as: 'alert_subscribe'
 
-  get 'unsubscribe', to: 'alert#emails', as: 'alert_emails'
-  post 'unsubscribe', to: 'alert#unsubscribe', as: 'alert_unsubscribe'
-
   get 'donators', to: 'donator#index', as: 'donator_index'
   get 'faq', to: 'static#faq', as: 'faq'
 
